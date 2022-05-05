fb7ceefd4e4affe125c9ca28db69d9611d0953c8
michael o'donovan <mikeodonovan3@gmail.com>
2022-05-05 14:03:02 +0100

    feat(App.js): Add psuedo selectors with Styled Components

--- src/App.js
+++ src/App.js
@@ -47,6 +47,30 @@ const Input = styled.input`
   border-radius: 3px;
 `
 
+const Thing = styled.div`
+  color: blue;
+
+    &:hover {
+      color: red; // <Thing> when hovered
+    }
+
+    & ~ & {
+      background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+    }
+
+    & + & {
+      background: lime; // <Thing> next to <Thing>
+    }
+
+    &.something {
+      background: orange; // <Thing> tagged with an additional CSS class ".something"
+    }
+
+    .something-else & {
+      border: 1px solid; // <Thing> inside another element labeled ".something-else"
+    }
+`
+
 function App() {
   return (
     <Wrapper>
@@ -79,6 +103,16 @@ function App() {
       <h3>You can pass props to a Styled Component and to the DOM:</h3>
       <Input defaultValue="hello" types="text"/>
       <Input defaultValue="welcome" type="text" inputColor="rebeccapurple"/>
+
+      <h3>Combining with CSS pseudoelements, pseudoselectors, and nesting:</h3>
+      <Thing>Hello world!</Thing>
+      <Thing>How ya doing?</Thing>
+      <Thing className="something">The sun is shining...</Thing>
+      <div>Pretty nice day today.</div>
+      <Thing>Don't you think?</Thing>
+      <div className="something-else">
+        <Thing>Splendid.</Thing>
+      </div>
 
     </Wrapper>
   );
